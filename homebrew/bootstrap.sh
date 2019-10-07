b0bfd9fbf48f7ee36f178e1aa0609718cef1bbe9
Codesign <cs@csdev.io>
2019-10-07 23:20:22 +0100

    Removed commented out code

--- homebrew/bootstrap.sh
+++ homebrew/bootstrap.sh
@@ -3,7 +3,7 @@ set -e
 
 # If homebrew is not installed, skip this section
 command_exists brew || {
-    warning "Homebrew is not installed so skipping taps."
+    warning "Homebrew is not installed. Skipping taps."
     return 0
 }
 
@@ -17,20 +17,9 @@ line "Tapping Homebrew repositories..."
 brew tap 'homebrew/core'
 brew tap 'homebrew/cask'
 brew tap 'homebrew/bundle'
-# HOMEBREW_TAP_BUNDLE_DIR=$(mktemp -d)
-# HOMEBREW_TAP_BUNDLE=$(mktemp "$HOMEBREW_TAP_BUNDLE_DIR"/brew-tap-bundle-XXXXXXXX)
-# echo "tap 'homebrew/core'" >> $HOMEBREW_TAP_BUNDLE
-# echo "tap 'homebrew/cask'" >> $HOMEBREW_TAP_BUNDLE
 
 # If we are on OS X, add homebrew/services as well
-[[ $MACOS ]] && brew tap 'homebrew/services' #echo "tap 'homebrew/services'" >> $HOMEBREW_TAP_BUNDLE
-
-# Install the taps
-# brew bundle -v --file=- < $HOMEBREW_TAP_BUNDLE
-
-# # Cleanup
-# [[ -f $HOMEBREW_TAP_BUNDLE ]] && rm -f $HOMEBREW_TAP_BUNDLE
-# [[ -d $HOMEBREW_TAP_BUNDLE_DIR ]] && rm -rf $HOMEBREW_TAP_BUNDLE_DIR
+[[ $MACOS ]] && brew tap 'homebrew/services'
 
 # Run a brew update
 line "Updating Homebrew..."
