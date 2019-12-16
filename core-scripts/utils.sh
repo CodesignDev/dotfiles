950d8d937191bd2e52e5b7bc9b69e8995f3bbb37
Codesign <cs@csdev.io>
2019-12-16 16:03:21 +0000

    Use the variable in the utility function

--- core-scripts/utils.sh
+++ core-scripts/utils.sh
@@ -21,5 +21,5 @@ is_interactive_shell() {
 
 github_get_latest_release_version() {
     REPO=$1
-    curl -fsSL "https://api.github.com/repos/$1/releases/latest" | jq '.tag_name'
+    curl -fsSL "https://api.github.com/repos/$REPO/releases/latest" | jq '.tag_name'
 }
