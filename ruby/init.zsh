1429dbe9396b55333e46e52a78dc2f80e669e50d
Codesign <cs@csdev.io>
2020-04-06 00:07:03 +0100

    Split ruby init script into a separate path file

--- ruby/init.zsh
+++ ruby/init.zsh
@@ -1,8 +1,2 @@
-# rbenv directory
-export RBENV_DIR="$HOME/.rbenv"
-
-# Add rbenv's bin directory to path if this is a manual install
-[[ -d "$RBENV_DIR/bin" ]] && export PATH="$RBENV_DIR/bin:$PATH"
-
 # Set up rbenv in current shell
 eval "$(rbenv init -)"
---
+++ ruby/path.zsh
@@ -0,0 +1,5 @@
+# rbenv directory
+export RBENV_DIR="$HOME/.rbenv"
+
+# Add rbenv's bin directory to path if this is a manual install
+[[ -d "$RBENV_DIR/bin" ]] && export PATH="$RBENV_DIR/bin:$PATH"
