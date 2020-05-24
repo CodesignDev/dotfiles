6b9262af2a3c3eb367685027b150ea607f061a9d
Codesign <cs@csdev.io>
2020-05-24 20:35:12 +0100

    Add new packages command that wraps other commands

--- core-scripts/packages-core.sh
+++ core-scripts/packages-core.sh
@@ -1,5 +1,67 @@
 #!/usr/bin/env bash
 
+packages() {
+    local COMMAND=$1
+    local ARGS=${@:2}
+
+    local PACKAGE_MANAGER_COMMAND=package_manager_cmd_exec
+    local PACKAGE_COMMAND
+
+    case $COMMAND in
+
+        # Install package(s)
+        install)
+            PACKAGE_COMMAND=install_package
+            ;;
+
+        # Install package group
+        install_group)
+            PACKAGE_COMMAND=install_package_group
+            ;;
+
+        # Update package managers
+        update)
+            PACKAGE_COMMAND=update_packages
+            ;;
+
+        # Upgrade package(s)
+        upgrade)
+            PACKAGE_COMMAND=upgrade_packages
+            ;;
+
+        # Add package repository
+        add_repository)
+            PACKAGE_COMMAND=add_repository
+            ;;
+
+        # Is package installed?
+        is_installed)
+            PACKAGE_COMMAND=is_package_installed
+            ;;
+
+        # List files for a package
+        list_files)
+            PACKAGE_COMMAND=list_package_files
+            ;;
+
+        # ----------------------------------------
+
+        # Restrict command to particular package manager(s)
+        restrict)
+            PACKAGE_MANAGER_COMMAND=restrict_package_managers
+            ;;
+
+        # Unknown command
+        *)
+            warning "Unknown command to packages"
+            return 1
+
+    esac
+
+    # Run the command
+    $PACKAGE_MANAGER_COMMAND $PACKAGE_COMMAND ${ARGS[@]}
+}
+
 install_package() {
     package_manager_cmd_exec install_package $@
 }
