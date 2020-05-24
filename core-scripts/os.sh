2a5615b4276e9b308f1b133229a9c08bccf199b1
Codesign <cs@csdev.io>
2020-05-24 20:44:16 +0100

    Change OS system to use flags instead of variables

--- core-scripts/os.sh
+++ core-scripts/os.sh
@@ -4,41 +4,55 @@
 export OS=
 export MACOS=0
 export LINUX=0
-export WSL=0
-export WSL2=0
 export WINDOWS=0
+export WSL_VERSION=0
 
 # Detect the OS
 detect_os() {
-    is_macos && export MACOS=1 && export UNIX=1
-    is_linux && export LINUX=1 && export UNIX=1
-    is_wsl && export WSL=1
-    is_wsl2 && export WSL2=1
-    is_windows && export WINDOWS=1
 
-    is_macos && export OS="darwin"
-    is_linux && export OS="linux"
-    is_windows && export OS="windows"
+    # Get the os details
+    OS_KERNEL_NAME=$(uname -s)
+    OS_KERNEL_RELEASE=$(uname -r)
+
+    # Extract base OS
+    [[ "$OS_KERNEL_NAME" == "Darwin" ]] && export MACOS=1 && export OS="darwin"
+    [[ "$OS_KERNEL_NAME" == "Linux" ]] && export LINUX=1 && export OS="linux"
+    [[ "$OS_KERNEL_NAME" == *"_NT"* ]] && export WINDOWS=1 && export OS="windows"
+
+    # Detect WSL runtime version
+    [[ "$OS_KERNEL_RELEASE" == *"microsoft"* ]] && export WSL_VERSION=1
+    [[ "$OS_KERNEL_RELEASE" == *"microsoft-standard"* ]] && export WSL_VERSION=2
+
+    # Set some flags
+    (is_macos || is_linux) && export UNIX=1
 
     return 0
 }
 
 is_macos() {
-    [[ $(uname -s) == "Darwin" ]]
+    [[ "$MACOS" == "1" ]]
 }
 
 is_linux() {
-    [[ $(uname -s) == "Linux" ]]
-}
-
-is_wsl() {
-    is_linux && [[ $(uname -r | grep $QUIET_FLAG_GREP -i "microsoft") ]]
-}
-
-is_wsl2() {
-    is_wsl && [[ $(uname -r | grep $QUIET_FLAG_GREP -i "microsoft-standard") ]]
+    [[ "$LINUX" == "1" ]]
 }
 
 is_windows() {
-    [[ $(uname -s | grep $QUIET_FLAG_GREP "_NT-") ]]
+    [[ "$WINDOWS" == "1" ]]
+}
+
+is_unix() {
+    is_macos || is_linux
+}
+
+is_wsl() {
+    is_linux && [[ $WSL_VERSION -gt 0 ]]
+}
+
+is_wsl1() {
+    is_linux && [[ "$WSL_VERSION" == "1" ]]
+}
+
+is_wsl2() {
+    is_linux && [[ "$WSL_VERSION" == "2" ]]
 }
