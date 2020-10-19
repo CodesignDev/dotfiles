8c5ffb5869e703b89eccd211af05db8b187ffb12
Codesign <cs@csdev.io>
2020-10-20 00:23:31 +0100

    Changed python version filter to ignore rc versions

--- python/post-install.sh
+++ python/post-install.sh
@@ -7,8 +7,8 @@ DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if command_exists pyenv; then
 
     # Get the latest python 2 and 3 versions currently availble
-    PYTHON_3_LATEST_VERSION=$(pyenv install --list | sed 's/^  //' | grep -v - | grep -v 'dev\|a\|b' | grep '^3' | tail -1)
-    PYTHON_2_LATEST_VERSION=$(pyenv install --list | sed 's/^  //' | grep -v - | grep -v 'dev\|a\|b' | grep '^2' | tail -1)
+    PYTHON_3_LATEST_VERSION=$(pyenv install --list | sed 's/^  //' | grep -v - | grep -v 'dev\|a\|b\|rc' | grep '^3' | tail -1)
+    PYTHON_2_LATEST_VERSION=$(pyenv install --list | sed 's/^  //' | grep -v - | grep -v 'dev\|a\|b\|rc' | grep '^2' | tail -1)
 
     # Install the latest v3 version if it isn't already installed
     pyenv versions --bare | grep $QUIET_FLAG_GREP $PYTHON_3_LATEST_VERSION || {
